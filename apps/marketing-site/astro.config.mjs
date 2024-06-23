03da4b7369c56d1bac8481e584d2b8fef5ac32fb
plushdohn <lucafarci@protonmail.com>
2024-06-23 23:01:22 +0200

    chore(marketing-site): setup adapter

--- apps/marketing-site/astro.config.mjs
+++ apps/marketing-site/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: "server",
+  adapter: vercel()
 });
