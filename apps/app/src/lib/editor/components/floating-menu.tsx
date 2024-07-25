b1587122d5c16792b013b8ef5208e8afca2a7d25
plushdohn <lucafarci@protonmail.com>
2024-07-25 21:43:52 +0200

    feat(app): inline color editing

--- apps/app/src/lib/editor/components/floating-menu.tsx
+++ apps/app/src/lib/editor/components/floating-menu.tsx
@@ -1,13 +1,13 @@
 import { JSX, Show, createMemo } from "solid-js";
 import {
   LinkIcon,
-  SendIcon,
   Trash2Icon,
-  SaveAllIcon,
   ArrowUpNarrowWideIcon,
   TextIcon,
   CircleCheckBigIcon,
   SparklesIcon,
+  TypeIcon,
+  PaintBucketIcon,
 } from "lucide-solid";
 
 import { Input } from "~/lib/ui/components/input";
@@ -17,6 +17,7 @@ import { Textarea } from "~/lib/ui/components/textarea";
 import { useMutation } from "~/lib/ui/hooks/useMutation";
 import { editTemplateElement } from "~/lib/templates/actions";
 import { PopoverContent, PopoverRoot } from "~/lib/ui/components/popover";
+import ColorPicker from "./color-picker";
 
 type Props = {
   projectId: string;
@@ -89,6 +90,13 @@ export function FloatingMenu(props: Props) {
     return elements;
   });
 
+  const color =
+    props.element.computedStyleMap().get("color")?.toString() || "#000000";
+
+  const backgroundColor =
+    props.element.computedStyleMap().get("background-color")?.toString() ||
+    "transparent";
+
   return (
     <PopoverRoot
       open
@@ -126,76 +134,114 @@ export function FloatingMenu(props: Props) {
               )}
             />
           </form>
-          <div class="flex gap-1 w-full justify-end mt-1.5">
-            <Button
-              icon={() => <ArrowUpNarrowWideIcon class="size-4" />}
-              variant="ghost"
-              onClick={() =>
-                props.onChangeSelection(props.element.parentElement!)
+
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const element = props.element as HTMLElement;
+
+              const formData = new FormData(e.target as HTMLFormElement);
+
+              element.removeAttribute("data-selected");
+
+              if (formData.get("src")) {
+                (element as HTMLImageElement).src = formData.get(
+                  "src"
+                ) as string;
               }
-              aria-label="Select parent element"
-              even
-            />
-            <Button
-              icon={() => <Trash2Icon class="size-4" />}
-              color="destructive"
-              onClick={props.onDelete}
-              aria-label="Delete element"
-              even
-              variant="ghost"
-            />
-          </div>
-          <Show when={elementSettingsElements().length}>
-            <div class="flex flex-col gap-1">
-              <div class="flex gap-2 items-center w-full">
-                <hr class="grow border-gray-200" />
-                <span class="text-sm font-medium text-gray-500">
-                  Element settings (&lt;{props.element.tagName.toLowerCase()}
-                  &gt;)
-                </span>
-                <hr class="grow border-gray-200" />
-              </div>
-              <form
-                class="flex flex-col gap-2"
-                onSubmit={(e) => {
-                  e.preventDefault();
 
-                  const element = props.element as HTMLElement;
+              if (formData.get("href")) {
+                (element as HTMLAnchorElement).href = formData.get(
+                  "href"
+                ) as string;
+              }
 
-                  const formData = new FormData(e.target as HTMLFormElement);
+              if (formData.get("contents")) {
+                element.textContent = formData.get("contents") as string;
+              }
 
-                  element.removeAttribute("data-selected");
+              if (formData.get("textColor")) {
+                element.style.color = formData.get("textColor") as string;
+              }
 
-                  if (formData.get("src")) {
-                    (element as HTMLImageElement).src = formData.get(
-                      "src"
-                    ) as string;
+              if (formData.get("backgroundColor")) {
+                element.style.backgroundColor = formData.get(
+                  "backgroundColor"
+                ) as string;
+              }
+
+              props.onComplete(element.outerHTML);
+            }}
+          >
+            <div class="flex gap-1 w-full justify-between mt-1.5">
+              <div class="flex gap-1 items-center">
+                <Show
+                  when={
+                    props.element instanceof HTMLParagraphElement ||
+                    props.element instanceof HTMLHeadingElement ||
+                    props.element instanceof HTMLAnchorElement ||
+                    props.element instanceof HTMLTableCellElement
                   }
-
-                  if (formData.get("href")) {
-                    (element as HTMLAnchorElement).href = formData.get(
-                      "href"
-                    ) as string;
-                  }
-
-                  if (formData.get("contents")) {
-                    element.textContent = formData.get("contents") as string;
-                  }
-
-                  props.onComplete(element.outerHTML);
-                }}
-              >
-                {elementSettingsElements()}
-                <Button
-                  type="submit"
-                  icon={() => <CircleCheckBigIcon class="size-4" />}
-                  class="self-end"
                 >
-                  Save
-                </Button>
-              </form>
+                  <ColorPicker
+                    icon={() => <TypeIcon class="size-4" />}
+                    aria-label="Change text color"
+                    value={color}
+                    name="textColor"
+                  />
+                  <ColorPicker
+                    icon={() => <PaintBucketIcon class="size-4" />}
+                    aria-label="Change background color"
+                    value={backgroundColor}
+                    name="backgroundColor"
+                  />
+                </Show>
+              </div>
+
+              <div class="flex gap-1 items-center">
+                <Button
+                  icon={() => <ArrowUpNarrowWideIcon class="size-4" />}
+                  variant="outline"
+                  onClick={() =>
+                    props.onChangeSelection(props.element.parentElement!)
+                  }
+                  aria-label="Select parent element"
+                  even
+                />
+                <Button
+                  icon={() => <Trash2Icon class="size-4" />}
+                  color="destructive"
+                  onClick={props.onDelete}
+                  aria-label="Delete element"
+                  even
+                  variant="outline"
+                />
+              </div>
             </div>
-          </Show>
+            <Show when={elementSettingsElements().length}>
+              <div class="flex flex-col gap-1">
+                <div class="flex gap-2 items-center w-full">
+                  <hr class="grow border-gray-200" />
+                  <span class="text-sm font-medium text-gray-500">
+                    &lt;{props.element.tagName.toLowerCase()}
+                    &gt; settings
+                  </span>
+                  <hr class="grow border-gray-200" />
+                </div>
+                <div class="flex flex-col gap-2">
+                  {elementSettingsElements()}
+                  <Button
+                    type="submit"
+                    icon={() => <CircleCheckBigIcon class="size-4" />}
+                    class="self-end"
+                  >
+                    Save
+                  </Button>
+                </div>
+              </div>
+            </Show>
+          </form>
         </>
       </PopoverContent>
     </PopoverRoot>
