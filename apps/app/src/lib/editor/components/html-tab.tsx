3c4cbd8f952cd266787224bbda78ec3e6c4a66c4
plushdohn <lucafarci@protonmail.com>
2024-08-06 14:05:36 +0200

    chore(editor): change html syntax highlighting

--- apps/app/src/lib/editor/components/html-tab.tsx
+++ apps/app/src/lib/editor/components/html-tab.tsx
@@ -4,14 +4,14 @@ import { createHighlighterCore } from "shiki/core";
 
 async function createCodeHighlight(code: string) {
   const highlighter = await createHighlighterCore({
-    themes: [import("shiki/themes/min-light.mjs")],
+    themes: [import("shiki/themes/vitesse-light.mjs")],
     langs: [import("shiki/langs/html.mjs")],
     loadWasm: getWasm,
   });
 
   return highlighter.codeToHtml(code, {
     lang: "html",
-    theme: "min-light",
+    theme: "vitesse-light",
   });
 }
 
