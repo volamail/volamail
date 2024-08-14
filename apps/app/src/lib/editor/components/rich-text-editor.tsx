78fce4a7a2aa34d8807bdaf931a6ff5900b13a04
plushdohn <lucafarci@protonmail.com>
2024-08-14 08:56:32 +0200

    chore(editor): alpha warn about rich text editing

--- apps/app/src/lib/editor/components/rich-text-editor.tsx
+++ apps/app/src/lib/editor/components/rich-text-editor.tsx
@@ -1,5 +1,5 @@
 import { Editor } from "@tiptap/core";
-import { TypeIcon } from "lucide-solid";
+import { InfoIcon, TypeIcon } from "lucide-solid";
 import Link from "@tiptap/extension-link";
 import Color from "@tiptap/extension-color";
 import StarterKit from "@tiptap/starter-kit";
@@ -85,28 +85,33 @@ export default function RichTextEditor(props: Props) {
   });
 
   return (
-    <div class="flex flex-col border border-gray-200 rounded-lg">
-      <div class="flex gap-2 border-b border-gray-200 p-1.5">
-        <Show when={editor()}>
-          {(editor) => (
-            <ColorPicker
-              icon={() => <TypeIcon class="size-4" />}
-              aria-label="Change text color"
-              value={editor().getAttributes("textStyle").color || ""}
-              onChange={(value) => editor().chain().setColor(value).run()}
-              name="textColor"
-            />
-          )}
-        </Show>
+    <div class="flex flex-col gap-1">
+      <div class="flex flex-col border border-gray-200 rounded-lg">
+        <div class="flex gap-2 border-b border-gray-200 p-1.5">
+          <Show when={editor()}>
+            {(editor) => (
+              <ColorPicker
+                icon={() => <TypeIcon class="size-4" />}
+                aria-label="Change text color"
+                value={editor().getAttributes("textStyle").color || ""}
+                onChange={(value) => editor().chain().setColor(value).run()}
+                name="textColor"
+              />
+            )}
+          </Show>
+        </div>
+        <div
+          id="rich-editor"
+          class="revert-tailwind rounded-b-lg [&>div]:p-4 [&>div]:rounded-b-lg"
+          style={{
+            background: props.backgroundColor,
+          }}
+        />
+        <input type="hidden" name="contents" value={contents()} />
       </div>
-      <div
-        id="rich-editor"
-        class="revert-tailwind rounded-b-lg [&>div]:p-4 [&>div]:rounded-b-lg"
-        style={{
-          background: props.backgroundColor,
-        }}
-      />
-      <input type="hidden" name="contents" value={contents()} />
+      <p class="text-xs text-gray-500 inline-flex items-center gap-1">
+        <InfoIcon class="size-4" /> Rich text editing is still in alpha.
+      </p>
     </div>
   );
 }
