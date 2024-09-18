07dd94591db96b7945fa0e2ef35330566b5ad88e
plushdohn <lucafarci@protonmail.com>
2024-09-18 18:12:25 +0200

    chore: local docker setup

--- apps/app/src/lib/analytics.ts
+++ apps/app/src/lib/analytics.ts
@@ -2,73 +2,73 @@ import { PostHog } from "posthog-node";
 import { env } from "~/lib/environment/env";
 
 const client =
-  "POSTHOG_API_KEY" in env
-    ? new PostHog(env.POSTHOG_API_KEY, {
-        host: "https://eu.i.posthog.com",
-        flushAt: 1,
-        flushInterval: 0,
-      })
-    : null;
+	"POSTHOG_API_KEY" in env && !import.meta.env.DEV
+		? new PostHog(env.POSTHOG_API_KEY, {
+				host: "https://eu.i.posthog.com",
+				flushAt: 1,
+				flushInterval: 0,
+			})
+		: null;
 
 export async function captureUserLoggedInEvent(user: {
-  id: string;
-  email: string;
+	id: string;
+	email: string;
 }) {
-  client?.capture({
-    distinctId: user.id,
-    event: "user_logged_in",
-    properties: {
-      email: user.email,
-    },
-  });
+	client?.capture({
+		distinctId: user.id,
+		event: "user_logged_in",
+		properties: {
+			email: user.email,
+		},
+	});
 
-  await client?.shutdown();
+	await client?.shutdown();
 }
 
 export async function captureUserRegisteredEvent(user: {
-  id: string;
-  email: string;
+	id: string;
+	email: string;
 }) {
-  client?.capture({
-    distinctId: user.id,
-    event: "user_registered",
-    properties: {
-      email: user.email,
-    },
-  });
+	client?.capture({
+		distinctId: user.id,
+		event: "user_registered",
+		properties: {
+			email: user.email,
+		},
+	});
 
-  await client?.shutdown();
+	await client?.shutdown();
 }
 
 export async function capturePlanPurchaseEvent(team: {
-  id: string;
-  name: string;
+	id: string;
+	name: string;
 }) {
-  client?.capture({
-    distinctId: team.id,
-    event: "plan_purchased",
-    properties: {
-      team_name: team.name,
-    },
-  });
+	client?.capture({
+		distinctId: team.id,
+		event: "plan_purchased",
+		properties: {
+			team_name: team.name,
+		},
+	});
 
-  await client?.shutdown();
+	await client?.shutdown();
 }
 
 export async function captureAiRequestEvent(userId: string) {
-  client?.capture({
-    distinctId: userId,
-    event: "ai_request",
-  });
+	client?.capture({
+		distinctId: userId,
+		event: "ai_request",
+	});
 
-  await client?.shutdown();
+	await client?.shutdown();
 }
 
 export async function captureEmailSentEvent(teamId: string) {
-  client?.capture({
-    event: "email_sent",
-    distinctId: teamId,
-  });
+	client?.capture({
+		event: "email_sent",
+		distinctId: teamId,
+	});
 
-  await client?.shutdown();
+	await client?.shutdown();
 }
