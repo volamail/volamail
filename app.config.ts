5bcdc7813175152c03168d9378cb729a39c549ee
plushdohn <lucafarci@protonmail.com>
2025-02-19 11:13:29 +0100

    chore: disable react compiler

--- app.config.ts
+++ app.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-const ReactCompilerConfig = {
-  target: "18",
-};
-
 export default defineConfig({
   vite: {
     plugins: [tsConfigPaths()],
   },
   server: {
     preset: "aws-lambda",
-  },
-  react: {
-    babel: {
-      plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
-    },
   },
 });
