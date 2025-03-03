f4892018fd27fec12bb5aadd0c6c101ef2d0de4b
plushdohn <lucafarci@protonmail.com>
2025-03-03 17:04:59 +0100

    feat: otp resend

--- app/routes/verify-otp.$email.tsx
+++ app/routes/verify-otp.$email.tsx
@@ -1,9 +1,12 @@
 import { authClient } from "@/modules/auth/client";
+import { Button } from "@/modules/ui/components/button";
 import { GridBgContainer } from "@/modules/ui/components/grid-bg-container";
 import { OtpInput } from "@/modules/ui/components/otp-input";
+import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { zodValidator } from "@tanstack/zod-adapter";
-import { useState } from "react";
+import { Redo2Icon } from "lucide-react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { z } from "zod";
 
@@ -20,30 +23,41 @@ function VerifyOtp() {
 	const params = Route.useParams();
 	const search = Route.useSearch();
 
-	const [verifying, setVerifying] = useState(false);
-
 	const navigate = useNavigate();
 
-	async function handleOtpSubmit(otp: string) {
-		setVerifying(true);
+	const verifyMutation = useMutation({
+		mutationFn: async (otp: string) => {
+			const result = await authClient.signIn.emailOtp({
+				email: params.email,
+				otp,
+			});
 
-		const result = await authClient.signIn.emailOtp({
-			email: params.email,
-			otp,
-		});
-
-		setVerifying(false);
-
-		if (result.error) {
+			if (result.error) {
+				throw result.error;
+			}
+		},
+		onError() {
 			toast.error("Verification failed");
+		},
+		async onSuccess() {
+			await navigate({
+				to: search.next ?? "/",
+			});
+		},
+	});
 
-			return;
-		}
+	const { countdown, restart: restartCountdown } = useCountdown(15);
 
-		navigate({
-			to: search.next ?? "/",
-		});
-	}
+	const resendMutation = useMutation({
+		mutationFn: async () => {
+			restartCountdown();
+
+			await authClient.emailOtp.sendVerificationOtp({
+				type: "sign-in",
+				email: params.email,
+			});
+		},
+	});
 
 	return (
 		<GridBgContainer className="flex h-dvh flex-col items-center justify-center bg-grid-small-black/10 p-4 dark:bg-grid-small-white/20">
@@ -55,7 +69,23 @@ function VerifyOtp() {
 					</p>
 				</div>
 
-				<OtpInput onComplete={handleOtpSubmit} pending={verifying} />
+				<div className="flex flex-col gap-4">
+					<OtpInput
+						onComplete={verifyMutation.mutate}
+						pending={verifyMutation.isPending}
+					/>
+
+					<Button
+						padding="lg"
+						color="neutral"
+						trailing={countdown === 0 && <Redo2Icon className="size-4" />}
+						loading={resendMutation.isPending}
+						onClick={() => resendMutation.mutate()}
+						disabled={countdown > 0}
+					>
+						Resend code {countdown > 0 && `(${countdown}s)`}
+					</Button>
+				</div>
 			</main>
 			<footer className="mt-4 text-gray-500 text-xs dark:text-gray-600">
 				<a
@@ -74,4 +104,21 @@ function VerifyOtp() {
 			</footer>
 		</GridBgContainer>
 	);
+}
+
+function useCountdown(seconds: number) {
+	const [countdown, setCountdown] = useState(seconds);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCountdown((prev) => prev - 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return {
+		countdown,
+		restart: () => setCountdown(seconds),
+	};
 }
