114712af084487ea55eec9a34a708134a0d6324d
plushdohn <lucafarci@protonmail.com>
2025-03-04 11:04:50 +0100

    fix: httpEquiv casing

--- app/modules/templates/render.tsx
+++ app/modules/templates/render.tsx
@@ -25,7 +25,7 @@ export function renderTemplateToHtml(template: {
 		<html lang="en">
 			<head>
 				<meta charSet="UTF-8" />
-				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="color-scheme" content="light" />
 
